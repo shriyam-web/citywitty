e1f2035daf2f0441ac16f6ac1fe7082069224ecf
shriyam-web <shriyamparashar5@gmail.com>
2025-11-07 19:10:26 +0530

    added an e-commerce

--- next.config.js
+++ next.config.js
@@ -4,22 +4,21 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-
-
-async rewrites() {
-  return [
-    {
-      source: '/:path*',
-      has: [
-        {
-          type: 'host',
-          value: 'partner.citywitty.com',
-        },
-      ],
-      destination: '/partner/:path*',
-    },
-  ];
-}
+  serverComponentsExternalPackages: ['razorpay'],
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'partner.citywitty.com',
+          },
+        ],
+        destination: '/partner/:path*',
+      },
+    ];
+  }
 };
 
 module.exports = nextConfig;
